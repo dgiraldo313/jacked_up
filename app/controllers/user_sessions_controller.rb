dcba67fc7f855722b96392b78086c3d6eed0849a
dgiraldo313 <dgiraldo@drew.edu>
2015-08-06 15:27:11 -0400

    added workout display styling

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -13,7 +13,7 @@ class UserSessionsController < ApplicationController
   		flash[:success] = "Thanks for logging in!"
   		redirect_to workouts_path
   	else 
-  		flash[:error] = "There was a problem logging in. Please check your email and password"
+  		flash[:error] = "There was a problem logging in. Please check your username and password"
   		render action: 'new'
   	end
   end
