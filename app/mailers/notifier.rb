5667fdc9d668b08604ec20bddfaf80b54911fc0e
dgiraldo313 <dgiraldo@drew.edu>
2015-09-10 22:20:20 -0400

    removed debugging hooks

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,10 +3,8 @@ class Notifier < ApplicationMailer
 	default from: "no-reply@jackedup.com"
 
 	def password_reset(user)
-		puts "Hello, logs 5!"
 		@user = user
 		mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>",
 		subject: "Reset Your Password")
-		puts "Hello, logs 6!"
 	end
 end
